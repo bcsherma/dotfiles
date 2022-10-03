294c73bc3d672b6297c5be2196f8ee34cb06b198
Ben Sherman <bcanfieldsherman@gmail.com>
2022-10-03 16:47:00 -0700

    moving to nvchad

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 if [ ! -d ~/.config/nvim ]; then
     mkdir ~/.config/nvim
+    git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 fi
 ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
 if [ -d ~/vim ]; then
